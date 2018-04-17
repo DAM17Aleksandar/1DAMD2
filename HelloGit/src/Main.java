561260cb15c208bd1ea78eb8a52d1d20f02fa4c5
davdriver <davidtabernerom@ejemplo.net>
2018-04-17 12:20:27 +0200

    Refactorized
    Refactorizado por David.

---
+++ HelloGit/src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hola, creado por Alex.");
+		System.out.println("Hola, ahora modificado y refactorizado por David.");
+
+	}
+
+}

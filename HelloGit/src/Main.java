34494f676e6495985d66a7ab0f79ee1b9ae378c2
ajuanjojjj <ajuanjojjj@gmail.com>
2018-04-17 12:32:23 +0200

    Añadido mi nombre y refactoriezado

--- HelloGit/src/Main.java
+++ HelloGit/src/Main.java
@@ -1,11 +1,8 @@
 
 public class Main {
 
-	public static void main(String[] args) {
-
+	public static void main(String[] args) 
+	{
 		System.out.println("Hola, creado por Alex.");
 		System.out.println("Hola, ahora modificado y refactorizado por David.");
-
-	}
-
-}
+		System.out.println("Adios, juankjo apruebva este codigo.");	}}
